f9136b104034239bc06e04ecd19fc654d1e33358
sulfur05 <riyacodes05@gmail.com>
2024-01-08 16:08:05 +0530

    added sponsor marquee

--- src/assets/components/sponsors.jsx
+++ src/assets/components/sponsors.jsx
@@ -1,12 +1,19 @@
+import Marquee from "react-fast-marquee";
+
 const Sponsor=()=>{
     return(
         <>
         <p className="height" color="black
-        ">
-            <div className="flex sponsors">
+        ">  
+        <div className="flex sponsors">
+                <Marquee direction="right" autoFill="true">
+                <div className="flex">
                 <img src="/images/sponsor1.png" alt="" />
                 <img src="/images/sponsor2.png" alt="" />
                 <img src="/images/sponsor3.png" alt="" />
+                </div>
+                </Marquee>
+                
             </div>
         </p>
         </>
